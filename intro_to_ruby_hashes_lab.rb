8f862859510869c1f73eb05f0e553dcf3a1e54e7
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-12 14:06:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,19 @@
 def monopoly
-	monopoly = {}
+	
 
 end
 
 def monopoly_with_second_tier
-	monopoly = {}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
 def monopoly_with_third_tier
-	monopoly = {}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
 def monopoly_with_fourth_tier
-	monopoly = {}#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
